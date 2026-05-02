12313b5740787eaa5f628026ec70d9f34729cfec
VASUDEV GUPTA <guptavasudev73@gmail.com>
2026-05-02 21:36:53 +0530

    this is second commit

--- dsu/dsu.cpp
+++ dsu/dsu.cpp
@@ -1,22 +1,26 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-class Dsu {
+class Dsu
+{
 public:
     vector<int> parent;
     vector<int> rank;
 
     // Constructor
-    Dsu(int n) {
+    Dsu(int n)
+    {
         parent.resize(n + 1);
         rank.resize(n + 1, 0);
 
-        for (int i = 0; i <= n; i++) {
+        for (int i = 0; i <= n; i++)
+        {
             parent[i] = i;
         }
     }
 
     // Find with Path Compression
-    int findParent(int node) {
+    int findParent(int node)
+    {
         if (parent[node] == node)
             return node;
 
@@ -24,32 +28,38 @@ public:
     }
 
     // Union by Rank
-    void union_op(int u, int v) {
+    void union_op(int u, int v)
+    {
         int pu = findParent(u);
         int pv = findParent(v);
 
         if (pu == pv)
             return;
 
-        if (rank[pu] < rank[pv]) {
+        if (rank[pu] < rank[pv])
+        {
             parent[pu] = pv;
         }
-        else if (rank[pu] > rank[pv]) {
+        else if (rank[pu] > rank[pv])
+        {
             parent[pv] = pu;
         }
-        else {
+        else
+        {
             parent[pv] = pu;
             rank[pu]++;
         }
     }
 
     // Check if two nodes are in same component (cycle detection helper)
-    bool check_cycle(int u, int v) {
+    bool check_cycle(int u, int v)
+    {
         return findParent(u) == findParent(v);
     }
 };
-int main(){
-    cout<<"Hello World";
+int main()
+{
+    cout << "Hello Worl babu" << endl;
+    cout<<"DSU implementation in C++" << endl;
     return 0;
-
 }
